ae3219cd2c225265866cf783087bc70700e7f9da
JustoRago <103001263+asdt560@users.noreply.github.com>
2022-09-05 18:22:21 -0300

    Add menu functionality

---
+++ navbar.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+function menuOpen() {
+    document.getElementById('overlay').style.display = 'flex';
+}
+  
+  // eslint-disable-next-line no-unused-vars
+  function menuClose() {
+    document.getElementById('overlay').style.display = 'none';
+}
