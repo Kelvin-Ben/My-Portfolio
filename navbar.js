0a789131bdb2aa5c009ef6cdce2c1659f5689394
Kelvin Ben <kevoben14@gmail.com>
2022-09-07 14:44:19 +0300

    Add JavaScript array to store all of the information for all projects

--- navbar.js
+++ navbar.js
@@ -19,7 +19,7 @@ const projects = [
     ],
     languages: [
       'html','css','javaScript',
-    ]
+    ],
     liveversion: 'placeholder',
     source: 'https://github.com/Kelvin-Ben/Desktop-Version-Portfolio',
     id:'1',
@@ -33,7 +33,7 @@ const projects = [
     ],
     languages: [
       'html','Ruby on rails','css','javascript',
-    ]
+    ],
     liveversion: 'placeholder',
     source: 'https://github.com/Kelvin-Ben/Desktop-Version-Portfolio',
     id: '2'
@@ -47,7 +47,7 @@ const projects = [
     ],
     languages: [
       'html','css','javaScript',
-    ]
+    ],
     liveversion: 'placeholder',
     source: 'https://github.com/Kelvin-Ben/Desktop-Version-Portfolio',
     id: '3',
@@ -55,30 +55,44 @@ const projects = [
   {
     name: 'Uber Navigation',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    featuredimage: './images/SnapshootPortfolio4.png"',
+    featuredimage: './images/SnapshootPortfolio4.png',
     technologies: [
       'Uber', 'Lead Developer', '2018',
     ],
     languages: [
       'html','Ruby on rails','css','javascript',
-    ]
+    ],
     liveversion: 'placeholder',
     source: 'https://github.com/Kelvin-Ben/Desktop-Version-Portfolio',
     id: '4',
   },
 ];
 const workcards = document.getElementById('Portfolio');
-console.log(Array.of(workcards))
-arr.forEach((item) => {
-  workcards.innerHTML += `<section id= ${item.id} class="workcards">
-  <img class="image" src=${item.featuredimage} alt="A Placeholder-image">
-  <div class="block">
-    <h2 class="tonic tonic1">${item.name}</h2>
-    <p class="paragraph">${item.description}</p>
-    <li class="tag">
-    </li>
-    <button class="projects">See Project</button>
-  </div>
-</section>`;
 
+projects.forEach((item) => {
+  workcards.innerHTML += `<article class="workcard" id=${item.id}>
+  <div>
+  <img src=${item.featuredimage} alt="" class="image">
+  </div>
+    <div class="block">
+      <h2 class="tonic1">Tonic</h2>
+      <div class="technologies">
+      <div class="frame canopy">CANOPY</div>
+      <div class="frame counter"></div>
+      <div class="frame backend">Back End Dev</div>
+      <div class="frame counter"></div>
+      <div class="frame year">2015</div>
+    </div>
+    <p class="paragraph">A daily selection of privately personalized reads; 
+      no accounts or sign-ups required.</p>
+      <div class="tags">
+        <ul>
+          <li class="tag html">html</li>
+          <li class="tag css">css</li>
+          <li class="tag javascript">javaScript</li>
+        </ul>
+      </div>
+      <button class="projects"><span class="see-projects">see projects</span></button>
+    </div>
+  </article>`;
 });
